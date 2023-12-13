09a095d75935d99bec19d0125b4c73fe5a587161
Noé Rio <79752407+noerio@users.noreply.github.com>
2023-12-14 00:15:06 +0100

    feat(display, runable): runable exercice.

--- src/variables.rs
+++ src/variables.rs
@@ -1,34 +1,30 @@
-
 // Fix the error below with least amount of modification to the code
 fn ex1() {
     let x: i32 = 5; // Uninitialized but used, ERROR !
     let y: i32 = 5; // Uninitialized but also unused, only a Warning !
 
     assert_eq!(x, y);
-    print!("Success!");
+    println!("Success!");
 }
-
 
 // Fill the blanks in the code to make it compile
 fn ex2() {
     let mut x = 1;
-    x += 2; 
-    
-    assert_eq!(x, 3);
-    print!("Success!");
-}
+    x += 2;
 
+    assert_eq!(x, 3);
+    println!("Success!");
+}
 
 // Fix the error below with least amount of modification
 fn ex3() {
     let x: i32 = 10;
     let y: i32 = 5;
     {
-        print!("The value of x is {} and value of y is {}", x, y);
+        println!("The value of x is {} and value of y is {}", x, y);
     }
-    print!("The value of x is {} and value of y is {}", x, y); 
+    println!("The value of x is {} and value of y is {}", x, y);
 }
-
 
 // Fix the error with the use of define_x
 fn ex4() {
@@ -37,9 +33,8 @@ fn ex4() {
 
 fn define_x() {
     let x = "hello";
-    print!("{}, world", x); 
+    println!("{}, world", x);
 }
-
 
 // Only modify `assert_eq!` to make the `println!` work(print `42` in terminal)
 fn ex5() {
@@ -52,9 +47,8 @@ fn ex5() {
     assert_eq!(x, 5);
 
     let x = 42;
-    print!("{}", x); // Prints "42".
+    println!("{}", x); // Prints "42".
 }
-
 
 // Remove a line in the code to make it compile
 fn ex6() {
@@ -63,20 +57,17 @@ fn ex6() {
     // Shadowing and re-binding
     x += 3;
 
-
     let y = 4;
     // Shadowing
-    let y = "I can also be bound to text!"; 
+    let y = "I can also be bound to text!";
 
-    print!("Success!");
+    println!("Success!");
 }
-
 
 // #[allow(unused_variables)] first solution
 fn ex7() {
     let _x = 1; // second solution "x" -> "_x"
 }
-
 
 // Fix the error below with least amount of modification
 fn ex8() {
@@ -86,29 +77,36 @@ fn ex8() {
     assert_eq!(x, 3);
     assert_eq!(y, 2);
 
-    print!("Success!");
+    println!("Success!");
 }
-
 
 fn ex9() {
     let (x, y);
-    (x,..) = (3, 4);
+    (x, ..) = (3, 4);
     [.., y] = [1, 2];
     // Fill the blank to make the code work
-    assert_eq!([x,y], [3, 2]);
+    assert_eq!([x, y], [3, 2]);
 
-    print!("Success!");
+    println!("Success!");
 }
 
-
-fn main() {
-    println!("Exercice 01: {}", ex1());
-    println!("Exercice 02: {}", ex2());
-    println!("Exercice 03: {}", ex3());
-    println!("Exercice 04: {}", ex4());
-    println!("Exercice 05: {}", ex5());
-    println!("Exercice 06: {}", ex6());
-    println!("Exercice 07: {}", ex7());
-    println!("Exercice 08: {}", ex8());
-    println!("Exercice 09: {}", ex9());
+pub fn variables_ex() {
+    println!("Exercice 01: ");
+    ex1();
+    println!("\nExercice 02: ");
+    ex2();
+    println!("\nExercice 03: ");
+    ex3();
+    println!("\nExercice 04: ");
+    ex4();
+    println!("\nExercice 05: ");
+    ex5();
+    println!("\nExercice 06: ");
+    ex6();
+    println!("\nExercice 07: ");
+    ex7();
+    println!("\nExercice 08: ");
+    ex8();
+    println!("\nExercice 09: ");
+    ex9();
 }
